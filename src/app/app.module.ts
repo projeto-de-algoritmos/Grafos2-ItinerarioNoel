243a3923391f06b747029b9e6d86b29df384dfc0
oo7gabriel <gabrielfsacademico@gmail.com>
2023-05-14 22:08:46 -0300

    Adicionando componente principal e seus css
    Co-authored-by: weslley17w <weslley17w@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GrafoPrimComponent } from './shared/grafo-prim/grafo-prim.component';
+import { GrafoHeaderComponent } from './shared/grafo-header/grafo-header.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GrafoPrimComponent,
+    GrafoHeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
